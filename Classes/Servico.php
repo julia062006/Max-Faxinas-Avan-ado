8ef400fc902ccd4393b17754e189e2257f427e8e
Flávia Gabriella <flaviagabriella.cs@gmail.com>
2025-08-17 14:29:29 -0300

    Instalando o composer e biblioteca carbon

--- Classes/Servico.php
+++ Classes/Servico.php
@@ -7,20 +7,10 @@ class Servico
     private string $formaDePagamento;
     private string $tipoDeServico;
 
-    public function __construct( $formaDePagamento, $tipoDeServico)
+    public function __construct($formaDePagamento, $tipoDeServico)
     {
-   
         $this->formaDePagamento = $formaDePagamento;
         $this->tipoDeServico = $tipoDeServico;
-    }
-
-    public function setPreco(float $preco)
-    {
-        if ($preco > 0) {
-            $this->preco = $preco;
-        } else {
-            throw new Exception("Preço inválido");
-        }
     }
 
     public function getFormaDePagamento(): string
@@ -38,13 +28,4 @@ class Servico
         return $this->preco;
     }
 }
-
-$servico = new Servico(formaDePagamento: "cartao", tipoDeServico: "limpeza");
-
-// try {
-//   $servico->setPreco(-120);
-//    echo "Preço: " . $servico->getPreco() . ". Forma de pagamento: " . $servico->getFormaDePagamento() . ". Tipo de serviço: " . $servico->getTipoDeServico();
-//} catch (Exception $e) {
-//    echo "Erro: " . $e->getMessage();
-// }
 

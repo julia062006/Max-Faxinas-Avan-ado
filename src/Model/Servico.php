547c7d7fd589ac1754aa108e8dc2275860bc4cde
Flávia Gabriella <flaviagabriella.cs@gmail.com>
2025-11-10 21:42:05 -0300

    Aplicando api de serviços

--- src/Model/Servico.php
+++ src/Model/Servico.php
@@ -1,16 +1,17 @@
 <?php
+
 namespace App\Model;
- 
+
 use App\Core\Database;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
 
- #[Entity()]
+#[Entity()]
 class Servico
 {
-     #[Column(), Id, GeneratedValue() ]
+    #[Column(), Id, GeneratedValue()]
     private int $id;
 
     #[Column()]
@@ -18,19 +19,26 @@ class Servico
 
     #[Column()]
     private string $tipoDeServico;
-   
 
-     public function __construct(string $tipoDeServico, float $preco)
+    #[Column()]
+    private string $imagem;
+
+    #[Column()]
+    private string $descricao;
+
+
+    public function __construct(string $tipoDeServico, float $preco, string $imagem, string $descricao)
     {
         $this->tipoDeServico = $tipoDeServico;
         $this->preco = $preco;
+        $this->imagem = $imagem;
+        $this->descricao = $descricao;
     }
 
-    public function getId() :int {
+    public function getId(): int
+    {
         return $this->id;
-        
     }
-    // Getters (Serve para pegar os valores)
 
     public function getTipoDeServico(): string
     {
@@ -42,7 +50,15 @@ class Servico
         return $this->preco;
     }
 
-    // Setters (Serve para alterar os valores)
+    public function getImagem(): string
+    {
+        return $this->imagem;
+    }
+
+    public function getDescricao(): string
+    {
+        return $this->descricao;
+    }
 
     public function setTipoDeServico(string $tipoDeServico): void
     {
@@ -54,13 +70,21 @@ class Servico
         $this->preco = $preco;
     }
 
-    //findall lê todas as tabelas do banco de dados
-    public static function findAll(): array 
+    public function setImagem(string $imagem): void
+    {
+        $this->imagem = $imagem;
+    }
+
+    public function setDescricao(string $descricao): void
+    {
+        $this->descricao = $descricao;
+    }
+
+    public static function findAll(): array
     {
         $entityManager = Database::getEntityManager();
         $repository = $entityManager->getRepository(Servico::class);
         return $repository->findAll();
-
     }
 
     public function save(): void
@@ -69,8 +93,4 @@ class Servico
         $em->persist($this);
         $em->flush();
     }
-    
 }
-
-
-
